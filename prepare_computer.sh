f3a8bc90f3248fba9b53564aa5510c53170278b4
Yoav Kleinberger <haarcuba@gmail.com>
2016-05-01 02:49:21 -0400

    new Ubuntu has apt and not aptitude

--- prepare_computer.sh
+++ prepare_computer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
-sudo aptitude safe-upgrade
-sudo aptitude install vim-gtk git build-essential terminator htop mc ipython openssh-server
+sudo apt upgrade
+sudo apt install vim-gtk git build-essential terminator htop mc ipython openssh-server
 
-printf  "yoav\tALL=(ALL)\tNOPASSWD: ALL\n" >> /etc/sudoers
+# printf  "yoav\tALL=(ALL)\tNOPASSWD: ALL\n" >> /etc/sudoers
 
