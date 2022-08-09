fd2154bd1d82da2cc536f2f400b96a1a995c6ab8
Yoav Kleinberger <haarcuba@gmail.com>
2022-08-09 20:01:50 +0300

    master-diff tool

---
+++ bin/master-diff.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import logging
+import uuid
+import subprocess
+
+def run(command, silent=False, **kwargs):
+    if silent:
+        kwargs['stdout'] = subprocess.DEVNULL
+    logging.info(f'run: {command}')
+    return subprocess.run(command, check=True, shell=True, **kwargs)
+
+def find_current_branch():
+    process = run('git branch --show-current', stdout=subprocess.PIPE, text=True)
+    return process.stdout.strip()
+
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+    starting_branch = find_current_branch()
+    logging.info(f'current branch: {starting_branch}')
+    temporary_branch = f'MASTER-DIFF-{uuid.uuid4()}'
+    run(f'git checkout -b {temporary_branch}', silent=True)
+    run(f"git merge origin/master -m 'MATER-DIFF AUTOMERGE'", silent=True)
+    run(f'git diff origin/master..HEAD')
+    run(f'git checkout {starting_branch}', silent=True)
+    run(f'git branch -D {temporary_branch}', silent=True)
+
+if __name__ == '__main__':
+    main()
