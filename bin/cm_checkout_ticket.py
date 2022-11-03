d11af59b188e4b1b22118fcd46aec9698501cf8a
Yoav Kleinberger <haarcuba@gmail.com>
2022-11-03 16:48:00 +0200

    yay

--- bin/cm_checkout_ticket.py
+++ bin/cm_checkout_ticket.py
@@ -5,15 +5,15 @@ import logging
 
 def run(command, **kwargs):
     logging.info(command)
-    subprocess.run(command, shell=True, check=True, **kwargs)
+    return subprocess.run(command, shell=True, check=True, **kwargs)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('search_string')
     arguments = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    process = run(f"gh pr list --search '{search_string}'", stdout=subprocess.PIPE, text=True)
-    lines = process.stdout.sprint('\n')
+    process = run(f"gh pr list --search '{arguments.search_string}'", stdout=subprocess.PIPE, text=True)
+    lines = process.stdout.split('\n')
     if len(lines) != 2:
         raise Exception('too many search results')
 
