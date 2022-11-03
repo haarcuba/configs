4f662661bffee4ac967555e03e6293837ce67b17
Yoav Kleinberger <haarcuba@gmail.com>
2022-11-03 16:45:03 +0200

    yay

---
+++ bin/cm_checkout_ticket.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import logging
+
+def run(command, **kwargs):
+    logging.info(command)
+    subprocess.run(command, shell=True, check=True, **kwargs)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('search_string')
+    arguments = parser.parse_args()
+    logging.basicConfig(level=logging.INFO)
+    process = run(f"gh pr list --search '{search_string}'", stdout=subprocess.PIPE, text=True)
+    lines = process.stdout.sprint('\n')
+    if len(lines) != 2:
+        raise Exception('too many search results')
+
+    pr_number = lines[0].split()[0]
+    run(f'gh pr checkout {pr_number}')
+
+
+if __name__ == '__main__':
+    main()
--- bin/master-diff.py
+++ bin/master-diff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import logging
 import uuid
+import argparse
 import subprocess
 
 def run(command, silent=False, **kwargs):
@@ -15,12 +16,15 @@ def find_current_branch():
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+    parser = argparse.ArgumentParser()
+    _, arguments = parser.parse_known_args()
     starting_branch = find_current_branch()
     logging.info(f'current branch: {starting_branch}')
     temporary_branch = f'MASTER-DIFF-{uuid.uuid4()}'
     run(f'git checkout -b {temporary_branch}', silent=True)
     run(f"git merge origin/master -m 'MATER-DIFF AUTOMERGE'", silent=True)
-    run(f'git diff origin/master..HEAD')
+    delegated_arguments = ' '.join(arguments)
+    run(f'git diff origin/master..HEAD {delegated_arguments}')
     run(f'git checkout {starting_branch}', silent=True)
     run(f'git branch -D {temporary_branch}', silent=True)
 
