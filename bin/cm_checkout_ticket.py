b83f609a03a6476417a469e9c1af64f79fbdf2a9
Yoav Kleinberger <haarcuba@gmail.com>
2022-11-06 15:22:52 +0200

    improve

--- bin/cm_checkout_ticket.py
+++ bin/cm_checkout_ticket.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import re
 import subprocess
 import logging
 
@@ -7,17 +8,35 @@ def run(command, **kwargs):
     logging.info(command)
     return subprocess.run(command, shell=True, check=True, **kwargs)
 
+def pr_title(pr_number):
+    process = run(f"gh pr view {pr_number}", stdout=subprocess.PIPE, text=True)
+    lines = process.stdout.split('\n')
+    TITLE = re.compile('title:\s+(?P<title>.*)')
+    match = TITLE.search(lines[0])
+    return match.groupdict()['title']
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('search_string')
+    parser.add_argument('--dry-run', action='store_true')
+    parser.add_argument('--yes', action='store_true', help='always checkout the pull request')
     arguments = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
     process = run(f"gh pr list --search '{arguments.search_string}'", stdout=subprocess.PIPE, text=True)
     lines = process.stdout.split('\n')
     if len(lines) != 2:
-        raise Exception('too many search results')
+        raise Exception(f'too many/not enough search results: {lines}')
 
     pr_number = lines[0].split()[0]
+    logging.info(f'found pull request: {pr_number}')
+    title = pr_title(pr_number)
+    logging.info(title)
+    if arguments.dry_run:
+        quit()
+    if not arguments.yes:
+        answer = input('go ahead? (Y/n) > ')
+        if answer.upper() not in ['yes', 'Yes', 'Y', 'y']:
+            quit()
     run(f'gh pr checkout {pr_number}')
 
 
