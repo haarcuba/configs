b6234eb155ee908d76a887fb5fe9a1d1b541b952
Yoav Kleinberger <haarcuba@gmail.com>
2023-01-30 22:50:52 +0200

    improve

--- bin/cm_branch.py
+++ bin/cm_branch.py
@@ -9,10 +9,11 @@ def run(command):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('message')
+    parser.add_argument('message', nargs='+')
     arguments = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    branch_name = arguments.message.replace(' ', '-').replace('.', '-')
+    message = '-'.join(arguments.message)
+    branch_name = message.replace(' ', '-').replace('.', '-')
     run(f'git checkout -b {branch_name}')
 
 
