2e0baa9921662919f4c819952f1055b08da11b7e
Yoav Kleinberger <haarcuba@gmail.com>
2026-01-18 01:12:30 +0200

    tidy up

--- vim/neovim/config.lua
+++ vim/neovim/config.lua
@@ -1,10 +1,5 @@
 -- Lua configuration for NeoVim
 
--- LSP Configuration using Neovim 0.11+ native vim.lsp.config
--- Install the LSP server: pip install python-lsp-server
--- Or use pyright: npm install -g pyright
-
--- Configure Python LSP (using pylsp - python-lsp-server)
 vim.lsp.config.pylsp = {
   cmd = { 'pylsp' },
   filetypes = { 'python' },
@@ -21,20 +16,12 @@ vim.lsp.config.pylsp = {
   }
 }
 
--- Enable pylsp for Python files
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'python',
   callback = function()
     vim.lsp.enable('pylsp')
   end,
 })
-
--- Alternative: pyright configuration (uncomment to use instead of pylsp)
--- vim.lsp.config.pyright = {
---   cmd = { 'pyright-langserver', '--stdio' },
---   filetypes = { 'python' },
---   root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
--- }
 
 -- LSP Keybindings
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -45,7 +32,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
